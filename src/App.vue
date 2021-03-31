1d6b2ec784c86f95a1a274d732661e2f9a78a2f1
Hama <hmms@gmail.com>
2021-03-31 18:29:50 +0200

    get request

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <add-blog>  </add-blog>
+    <show-blog>  </show-blog>
+
   </div>
 </template>
 
 <script>
 
 import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
+
 export default {
   components: {
-    'add-blog': addBlog
+    'add-blog': addBlog,
+    'show-blog': showBlog
+
   },
   data () {
     return{
@@ -23,8 +29,14 @@ export default {
 </script>
 
 <style>
-body{
-  margin: 0;
-  font-family: 'Nunito SemiBold';
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
 }
 </style>
---
+++ src/components/showBlog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="show-blog">
+    <h1>All Blog Articles</h1>
+    <div v-for="blog in blogs" class="single-blog" v-bind:key="blog" >
+      <h2>{{blog.title}}</h2>
+      <article>
+        {{blog.body}}
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return{
+      blogs:[]
+    }
+  },
+  methods: {
+
+  },
+  created() {
+   this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+     this.blogs = data.body.slice(0,10);
+   });
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
