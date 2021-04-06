ea2f7816ccfb10aec9c0b4b0ca57aace3c7fd8cd
Hama <hmms@gmail.com>
2021-04-06 11:39:33 +0200

    footer updated

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,15 +1,13 @@
 <template lang="html">
   <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
-  <!-- Grid container -->
-  <div class="container p-4"></div>
-  <!-- Grid container -->
 
-  <!-- Copyright -->
+  <div class="container p-4"></div>
+
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
     © 2021 Copyright:
-    <a class="text-white" href="https://github.com/Hama99o">My git hub</a>
+    <a class="text-white" href="https://github.com/Hama99o">Hama99o</a>
   </div>
-  <!-- Copyright -->
+
   </footer>
 </template>
 
