3b5ee9d6c3379aefdc57ab09cfc91eaf9b0cdd3c
Hama <hmms@gmail.com>
2021-04-19 14:59:21 +0200

    services/back moved to mixins back

--- src/mixins/back.js
+++ src/mixins/back.js
@@ -1,60 +1,60 @@
-import back from '../services/back.js'
+import axios from 'axios'
+
+const root_url = 'http://localhost:8080/'
 
 export default {
-
   created: function(){
     this.getArticle()
     this.getArticles()
   },
   methods:{
-   postOrPut: async function(){
-     if(this.$route.path == "/add" ) {
-       if (this.blog.title && this.blog.content) {
-         await back.post(this.blog)
-         this.submitted = true
-       }
-       this.errors =[]
-       if (!this.blog.title){
-         this.errors.push("title is empty")
-       }
-       if (!this.blog.content){
-         this.errors.push("content is empty")
-       }
-
-      }else {
-       if (this.blog.title && this.blog.content) {
-         // await this.$http.put('http://localhost:8080/api/articles/' + this.id, this.blog)
-         await back.put(this.id, this.blog)
-         this.submitted = true
-       }
-       this.errors =[]
-       if (!this.blog.title){
-         this.errors.push("title is empty")
-       }
-       if (!this.blog.content){
-         this.errors.push("content is empty")
-       }
+    async getArticles() {
+      const { data } = await axios.get(`${root_url}api/articles`)
+      var blogsArray = []
+      for(let key in data){
+        data[key].id = key
+        blogsArray.push(data[key])
+      }
+      this.blogs = blogsArray
+    },
+    async getArticle() {
+      if(this.$route.path != "/add" ) {
+        const { data } = await axios.get(`${root_url}api/articles/${this.id}`)
+        this.blog =  data
       }
     },
     destroyed: async function(){
       if(confirm('are you sure?'))
-      await  back.destroyArticle(this.id)
+      await axios.delete(`${root_url}api/articles/${this.id}`)
       return this.$router.push({path: '/'})
     },
-    async getArticle() {
-      if(this.$route.path != "/add" ) {
-       const article = await back.getArticle(this.id)
-       this.blog =  article
+   postOrPut: async function(){
+      if(this.$route.path == "/add" ) {
+        if (this.blog.title && this.blog.content) {
+          await axios.post(`${root_url}api/articles`, this.blog)
+          this.submitted = true
+        }
+        this.errors =[]
+        if (!this.blog.title){
+          this.errors.push("title is empty")
+        }
+        if (!this.blog.content){
+          this.errors.push("content is empty")
+        }
+
+        }else {
+        if (this.blog.title && this.blog.content) {
+          await axios.put(`${root_url}api/articles/${this.id}`, this.blog)
+          this.submitted = true
+        }
+        this.errors =[]
+        if (!this.blog.title){
+         this.errors.push("title is empty")
+        }
+        if (!this.blog.content){
+          this.errors.push("content is empty")
+        }
       }
-    },
-    async getArticles() {
-      const articles = await back.getArticles()
-      var blogsArray = []
-      for(let key in articles){
-        articles[key].id = key
-        blogsArray.push(articles[key])
-      }
-      this.blogs = blogsArray
     }
   }
 }
