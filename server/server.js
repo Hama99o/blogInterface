bf41de9336012ad1804ce3141c13c155a299fc2d
Caillou <igorlacroute@gmail.com>
2021-04-19 11:11:46 +0200

    move server's dependencies in main project & create start processes

--- server/server.js
+++ server/server.js
@@ -2,19 +2,21 @@ const axios = require('axios')
 const express = require('express')
 const router = require('./api/routes.js')
 const bodyParser = require('body-parser')
+const serveStatic = require('serve-static')
+const path = require('path')
 
 const app = express()
 const PORT = 5000
 
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
+// serve the static files built by vuejs
+app.use('/', serveStatic(path.join(__dirname, '..', 'dist')))
 
 app.use('/', router)
 
 app.listen(PORT)
+
+console.log(`Server running on port ${PORT}`)
