c3d8c9a4055975d5b563b5d264e1206c3c7d67a0
Hama <hmms@gmail.com>
2021-05-21 14:46:08 +0200

    error messages added

--- server/api/routes.js
+++ server/api/routes.js
@@ -6,13 +6,13 @@ module.exports = (app) => {
   app.post('/api/v1/articles', blogController.createArticle)
   app.delete('/api/v1/articles/:id', blogController.destroyArticle)
 
-  // app.use((error, req, res, next) => {
-  //   console.error(error.message)
-  //   console.error(JSON.stringify(error.response.data))
-  //
-  //   res.status(500).send({
-  //     message: error.message || error,
-  //     errorData: error.response.data
-  //   })
-  // })
+  app.use((error, req, res, next) => {
+    console.error(error.message)
+    console.error(JSON.stringify(error.response.data))
+
+    res.status(500).send({
+      message: error.message || error,
+      errorData: error.response.data
+    })
+  })
 }
