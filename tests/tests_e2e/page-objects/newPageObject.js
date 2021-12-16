756310fe6485748ca017beef52e53b1afd922440
hama <hmmshl@gmail.com>
2021-12-16 23:38:24 +0100

    test updated

--- tests/tests_e2e/page-objects/newPageObject.js
+++ tests/tests_e2e/page-objects/newPageObject.js
@@ -15,9 +15,19 @@ module.exports = {
       return this
         .setValue(selector, value)
     },
+    valueEasy (value) {
+      return this
+        .click(value)
+    },
+    selectLang (selector, value) {
+      return this
+        .click(selector)
+        .click(value)
+    },
     submitButton (value) {
       return this
         .click(value)
     }
+
   }]
 }
--- tests/tests_e2e/tests/new.js
+++ tests/tests_e2e/tests/new.js
@@ -4,11 +4,13 @@ module.exports = {
   'New page:' (browser) {
     const page = browser.page.newPageObject()
     const articleSave = '.article-save'
+    const article = '#single-article'
     page
       .navigate()
       .setTitle('@title', 'Testing the title.')
       .setContent('@content', 'Testing the content.')
-    page
       .submitButton('@submitButton')
+    browser
+      .assert.containsText(article, 'ihsan')
   }
 }
