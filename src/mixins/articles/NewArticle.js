5b54e02eae64ed4be3e895d915285f432432c80e
hama <hmmshl@gmail.com>
2021-12-18 16:13:21 +0100

    error message added

--- src/mixins/articles/NewArticle.js
+++ src/mixins/articles/NewArticle.js
@@ -3,17 +3,18 @@ import back from '@/services/Back.js'
 export default {
   methods: {
     async newArticle () {
-      try {
-        this.checked = 'loading'
-        const article = await back.postArticle(this.article)
+      this.checked = 'loading'
+      const article = await back.postArticle(this.article)
+      if (article.message) {
+        this.error = JSON.stringify(article.response.data.errorData)
+        this.checked = 'error'
+        await this.timeout(5000)
+        this.checked = 'save'
+      } else {
         this.checked = 'saved'
         await this.timeout(1000)
         this.checked = 'save'
         this.$router.push({ name: 'ShowArticle', params: { id: article.id } })
-      } catch {
-        this.checked = 'error'
-        await this.timeout(5000)
-        this.checked = 'save'
       }
     }
   }
--- src/services/Back.js
+++ src/services/Back.js
@@ -35,6 +35,7 @@ export default {
       return response.data
     } catch (error) {
       this.errorMethod(error)
+      return error
     }
   },
   getArticles (search, page, per) {
