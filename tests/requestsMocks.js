b59167208655f26cc1af682c1c9815f09e7a80ce
hama <hmmshl@gmail.com>
2021-12-18 12:39:41 +0100

    mock updated

--- tests/requestsMocks.js
+++ tests/requestsMocks.js
@@ -1,6 +1,6 @@
 const MockAdapter = require('axios-mock-adapter')
 const axios = require('axios')
-const factory = require('./factory.js')
+// const factory = require('./factory.js')
 
 const blog = require('../server/services/blog.js')
 const blogUrls = blog.blogUrls
@@ -11,18 +11,18 @@ const mockArticles = () => {
   // const articlesPaginated = blogUrls.articlesPaginated('', 0, 15) # paginate mock is not working so we are using articlesRegex
   const articleRegex = new RegExp(blogUrls.article('.+?'))
   // .passThrough()// for achieve real data
-  // mock.onGet(articleRegex).passThrough()
-  // mock.onGet(articlesRegex).passThrough()
-  // mock.onPost(articlesRegex).passThrough()
-  // mock.onPut(articleRegex).passThrough()
-  // mock.onDelete(articleRegex).passThrough()
+  mock.onGet(articleRegex).passThrough()
+  mock.onGet(articlesRegex).passThrough()
+  mock.onPost(articlesRegex).passThrough()
+  mock.onPut(articleRegex).passThrough()
+  mock.onDelete(articleRegex).passThrough()
 
-
-  mock.onGet(articleRegex).reply(200, factory.article)
-  mock.onGet(articlesRegex).reply(200, { articles: factory.articles(), meta: { page: 0, per: 15, nb_pages: 3, search: '' } })
-  mock.onPost(articlesRegex).reply(200, factory.article)
-  mock.onPut(articleRegex).reply(200, factory.article)
-  mock.onDelete(articleRegex).reply(200)
+  // for testing
+  // mock.onGet(articleRegex).reply(200, factory.article)
+  // mock.onGet(articlesRegex).reply(200, { articles: factory.articles(), meta: { page: 0, per: 15, nb_pages: 3, search: '' } })
+  // mock.onPost(articlesRegex).reply(200, factory.article)
+  // mock.onPut(articleRegex).reply(200, factory.article)
+  // mock.onDelete(articleRegex).reply(200)
 
 }
 module.exports = {
