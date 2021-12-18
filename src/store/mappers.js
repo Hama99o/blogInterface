ac42eb3b11854d13a54a664edc2f1f72a206e08e
hama <hmmshl@gmail.com>
2021-12-18 16:12:32 +0100

    error store removed

--- src/store/mappers.js
+++
@@ -1,8 +0,0 @@
-import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
-
-export default {
-  state: mapState,
-  getters: mapGetters,
-  mutations: mapMutations,
-  actions: mapActions
-}
